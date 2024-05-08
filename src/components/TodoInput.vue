31309c8fd99b3b173c8a6aca11dde2bc02932caa
iyeonghun <twosoull@naver.com>
2024-05-09 00:36:27 +0900

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    data : function(){
+        return{
+            newTodoItem : ""
+        }
+
+    },
+    methods : {
+        addTodo: function(){
+            //저장하는 로직
+            localStorage.setItem(this.newTodoItem,this.newTodoItem);
+            this.clearInput();
+        },
+        clearInput : function(){
+            this.newTodoItem = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+input:focus{
+    outline : none;
+}
+.inputBox {
+    background : white;
+    height: 50px;
+    line-height: 50px;ßßßer-radius : 5px;
+}
+.inputBox input{
+    background : none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float : right;
+    background : linear-gradient(to right, #6478FB, #8763FB);
+    display : block;
+    width : 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(todoItem,i) in todoItems" v-bind:key="i"
+            class="shadow">{{ todoItem }}
+            <button v-on:click="removeTodo(todoItem, i)" class="removeBtn">
+                <i class="removeBtn fas fa-trash-alt"></i>
+            </button>
+        </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data : function(){
+        return{
+            todoItems : []
+        }
+    },
+    created : function(){
+        if(localStorage.length > 0){
+            console.log(localStorage.length);            for(var i = 0; i < localStorage.length; i++){
+                this.todoItems.push(localStorage.key(i))
+                console.log(localStorage.key(i));   
+            }
+        }
+    },
+    methods : {
+        removeTodo : function(todoItem,index){
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  /* color: black; */
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  /* color: #62acde; */
+  color: black;
+}
+.textCompleted {
+  text-decoration: line-through;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+</style>
